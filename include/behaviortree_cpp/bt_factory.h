1280f4073afc49b999f82f8768b3acbf4fb5a4cb
Davide Faconti <davide.faconti@gmail.com>
2022-12-01 09:16:41 +0100

    WIP: debugging skins and observers

--- include/behaviortree_cpp/bt_factory.h
+++ include/behaviortree_cpp/bt_factory.h
@@ -90,9 +90,9 @@ public:
   struct Subtree
   {
     using Ptr = std::shared_ptr<Subtree>;
-    uint16_t uid;
     std::vector<TreeNode::Ptr> nodes;
     Blackboard::Ptr blackboard;
+    std::string instance_name;
   };
 
   std::vector<Subtree::Ptr> subtrees;
@@ -174,6 +174,12 @@ public:
   //Call the visitor for each node of the tree.
   void applyVisitor(const std::function<void(TreeNode*)>& visitor);
 
+  uint16_t assignUID(TreeNode& node) {
+    auto uid =  ++uid_counter_;
+    node.uid_ = uid;
+    return uid;
+  }
+
 private:
   std::shared_ptr<WakeUpSignal> wake_up_;
 
@@ -184,6 +190,8 @@ private:
   };
 
   NodeStatus tickRoot(TickOption opt, std::chrono::milliseconds sleep_time);
+
+  uint16_t uid_counter_ = 0;
 };
 
 class Parser;
